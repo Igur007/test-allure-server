86ea1447afdf7f9a78a4ef0b2bd67e3b8cc689ba
Ivan Huryn <igur007@gmail.com>
2024-11-13 14:54:04 +0200

    run tests for 2h

--- tests/unit/api/base/test_e2e.py
+++ tests/unit/api/base/test_e2e.py
@@ -1,6 +1,7 @@
+from time import sleep
+
 import allure
 import pytest
-
 from harness.api.base import base_requests
 
 
@@ -8,7 +9,7 @@ from harness.api.base import base_requests
 @allure.feature("e2e")
 class TestE2ERequests:
 
-    @pytest.mark.parametrize('execution_number', range(1, 3001))
+    @pytest.mark.parametrize('execution_number', range(1, 1115))
     def test(self, config, execution_number):
         br = base_requests.BaseRequests(config)
         for _ in range(10):
@@ -16,3 +17,4 @@ class TestE2ERequests:
                 "GET",
                 'https://cluster02.neosdata.cloud/api/gateway/v2/output/d669e53f-6fe3-4727-b0f9-e3be700da88f/journal',
             )
+            sleep(0.5)
